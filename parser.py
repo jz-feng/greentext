0eee113d8a49af165d301fb2ee730515cd3b0a47
Jerry Feng <jerry.feng41@gmail.com>
2015-10-18 17:17:47 -0400

    added for loops

--- parser.py
+++ parser.py
@@ -1,29 +1,77 @@
 import sys
+
 __author__ = 'Jerry'
 
 
-def main():
+def parse(lines):
     variables = {}
+    callstack = []
 
-    for line in sys.stdin:
-        if line[0] != '>':
-            print "do you even greentext"
-            return
-
-        tokens = line[1:].split()
+    linecount = 0
+    while linecount < len(lines):
+        tokens = lines[linecount].split()
         # print tokens
         tokenslen = len(tokens)
-
-        if tokens[0] == "mfw":
-            if tokenslen > 1:
-                if tokens[1].startswith('\'') and tokens[1].endswith('\''):
-                    print tokens[1][1:len(tokens[1]) - 1]
-                elif tokens[1] in variables:
-                    print variables[tokens[1]]
+        if tokenslen == 0:
+            continue
+        if (tokens[0] == "mfw") and (tokenslen > 1):
+            for token in tokens[1:]:
+                if (token.startswith('\'') and token.endswith('\'')) \
+                        or (token.startswith('\"') and token.endswith('\"')):
+                    print token[1:len(token) - 1],
+                elif token in variables:
+                    print variables[token],
+            print
 
         elif tokens[0] == "be":
             if tokenslen == 3:
-                variables[tokens[1]] = tokens[2]
+                if tokens[2].isdigit():
+                    variables[tokens[1]] = int(tokens[2])
+                else:
+                    try:
+                        variables[tokens[1]] = float(tokens[2])
+                    except ValueError:
+                        variables[tokens[1]] = tokens[2]
+                # elif tokenslen > 3:
+                # for i in range(1, tokenslen, 1):
+                # elif tokens[0] == "implying":
+
+        elif tokens[0] == "inb4":
+            if (tokenslen == 8) and (tokens[2] == "from") and (tokens[4] == "to") and (tokens[6] == "by"):
+                variables[tokens[1]] = int(tokens[3])
+                callstack.append({"linepos": linecount,
+                                  "counter": tokens[1],
+                                  "start": int(tokens[3]),
+                                  "end": int(tokens[5]),
+                                  "step": int(tokens[7])})
+        elif (tokens[0] == "done") and (tokenslen > 1):
+            if tokens[1] == "inb4":
+                call = callstack[-1]
+                variables[call["counter"]] += call["step"]
+                if (call["step"] > 0 and variables[call["counter"]] < call["end"]) \
+                        or (call["step"] < 0 and variables[call["counter"]] > call["end"]):
+                    linecount = call["linepos"]
+                else:
+                    del variables[call["counter"]]
+                    callstack.pop()
+
+
+        linecount += 1
+
+
+def main():
+    inputlines = []
+
+    for line in sys.stdin:
+        strippedline = line.lstrip()
+        if len(strippedline) > 0:
+            if strippedline[0] != '>':
+                print "do you even greentext"
+                return
+            else:
+                inputlines.append(strippedline[1:])
+
+    parse(inputlines)
 
 
 main()
