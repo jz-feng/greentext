af0c96d08a0d295d3022a0f871922cc99df735e7
Jerry Feng <jerry.feng41@gmail.com>
2017-01-09 13:48:14 -0500

    rename file + refactor class name, move utils functions out of main file

--- greentext.py
+++ greentext.py
@@ -1,5 +1,5 @@
 import sys
-import re
+from utils import *
 
 __author__ = 'Jerry'
 
@@ -9,70 +9,11 @@ FALSE = ":^("
 truefalse = [TRUE, FALSE]
 
 
-def is_token_literal(token):
-    return str(token).startswith('\'') and str(token).endswith('\'')
-
-
-def is_float(token):
-    try:
-        float(token)
-        return True
-    except ValueError:
-        return False
-
-
-def print_error(message, line_address):
-    if line_address == -1:
-        print "wtf:", message
-    else:
-        print "wtf:", message, "at line", line_address + 1
-
-
-def parse_literals(line):
-    tokens = [t for t in re.split(r"(\")", line) if len(t) > 0]
-    if len(tokens) > 0 and tokens[-1].endswith("\n"):   # Remove newline char
-        tokens[-1] = tokens[-1][:-1]
-    if len(tokens) > 1:
-        is_literal = False
-        i = 0
-        while i < len(tokens):
-            t = tokens[i]
-            if is_literal:
-                if t == "\"":
-                    if tokens[i - 1] != "\"":
-                        tokens[i - 1] += "'"    # Suffix with single quote
-                    is_literal = False
-                else:
-                    if tokens[i - 1] == "\"":
-                        tokens[i] = "'" + t     # Prefix with single quote
-                    else:
-                        # Combine current token with previous token, remove current token, don't increment index
-                        tokens[i - 1] = tokens[i - 1] + " " + t
-                        del tokens[i]
-                        i -= 1
-            else:
-                if t == "\"":
-                    is_literal = True
-            i += 1
-    # Remove "s from tokens stream
-    return [t for t in tokens if len(t) > 0 and t != "\""]
-
-
-def parse_tokens(tokens):
-    split_tokens = []
-    for t in tokens:
-        if is_token_literal(t):
-            split_tokens.append(t)
-        else:
-            split_tokens.extend(t.split())
-    return split_tokens
-
-
 class GreentextError(SyntaxError):
     """Custom Greentext syntax error"""
 
 
-class Parser:
+class Greentext:
 
     global_variables = {}
     functions = {}
@@ -698,4 +639,4 @@ class Parser:
         self.parse(inputlines)
 
 if __name__ == "__main__":
-    Parser().main()
+    Greentext().main()
